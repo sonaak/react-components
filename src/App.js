1c191bc224ed71d95456956e2442d63f8d970fcd
Knight Fu <knight.w.fu@gmail.com>
2017-06-30 20:41:31 -0400

    adds a toggle

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
-import Input from './form/Input.jsx';
+import IoIosEyeOutline from 'react-icons/lib/io/ios-eye-outline';
 import './App.css';
+
+class Toggle extends Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+
+    const states = Object.keys(props.map || {});
+    const activeState = props.default || states[0];
+
+    this.state = {
+      active: activeState,
+    }
+  }
+
+  render() {
+    const activeComponent = (this.props.map || {})[this.state.active] || {};
+    const component = activeComponent.component || null;
+    const props = activeComponent.props || null;
+    return (
+      <div className="sonaak-toggle">
+        { component ? component(...props) : null }
+        <IoIosEyeOutline size="100%" style={{
+          opacity: .2
+        }}/>
+      </div>
+    )
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Input label="first name"/>
-        <Input label="last name"/>
+        <Toggle />
       </div>
     );
   }
--- src/form/Input.jsx
+++ src/form/Input.jsx
@@ -1,5 +1,28 @@
 import React, { Component } from 'react';
 
+function handleBlur() {
+  this.setState({
+    focused: false
+  });
+}
+
+function handleFocus() {
+  this.setState({
+    focused: true
+  })
+}
+
+function handleChange(event) {
+  const target = event.target;
+  if (typeof this.props.onChange === "function") {
+    this.props.onChange(target.value, this.state.value, event, this);
+  }
+
+  this.setState({
+    value: target.value
+  });
+}
+
 /**
  * Input is a text field that has nothing but text in it.
  */
@@ -13,32 +36,9 @@ class Input extends Component {
       focused: false
     }
 
-    this.onChange = this.onChange.bind(this);
-    this.onFocus = this.onFocus.bind(this);
-    this.onBlur = this.onBlur.bind(this);
-  }
-
-  onChange(event) {
-    const target = event.target;
-    if (typeof this.props.onChange === "function") {
-      this.props.onChange(target.value, this.state.value, event, this);
-    }
-
-    this.setState({
-      value: target.value
-    });
-  }
-
-  onFocus() {
-    this.setState({
-      focused: true
-    });
-  }
-
-  onBlur() {
-    this.setState({
-      focused: false
-    });
+    this.onChange = handleChange.bind(this);
+    this.onFocus = handleFocus.bind(this);
+    this.onBlur = handleBlur.bind(this);
   }
 
   render() {
@@ -54,6 +54,7 @@ class Input extends Component {
           {this.state.label}
         </div>
         <input
+          type="text"
           onChange={this.onChange}
           onFocus={this.onFocus}
           onBlur={this.onBlur}
@@ -70,4 +71,50 @@ class Input extends Component {
   }
 }
 
-export default Input;
+class Password extends Component {
+  constructor(props) {
+    super(props)
+    this.props = props;
+    this.state = {
+      value: "",
+      label: props.label,
+      redacted: true,
+      focused: false
+    }
+
+    this.onChange = handleChange.bind(this);
+    this.onFocus = handleFocus.bind(this);
+    this.onBlur = handleBlur.bind(this);
+  }
+
+  render() {
+    const labelClassName = "label" +
+      (this.state.focused? " focused" : "") +
+      (this.state.value === "" ? "" : " hidden");
+    const activeLabelClassName = "active-label" +
+      (this.state.value === "" ? " hidden" : "");
+
+    return (
+      <div className="sonaak-text-input">
+        <div className={labelClassName}>
+          {this.state.label}
+        </div>
+        <input
+          type="password"
+          onChange={this.onChange}
+          onFocus={this.onFocus}
+          onBlur={this.onBlur}
+          value={this.state.value}
+        />
+        <div className="line"></div>
+        <div
+          className={activeLabelClassName}
+        >
+          {this.state.label}
+        </div>
+      </div>
+    );
+  }
+}
+
+export { Input, Password };
